e43d08f364c9d7978c4748a8f5e3a1f4367896e0
DamVanAnh <vananhdam@gmail.com>
2026-02-02 18:16:13 +0700

    feat: send message with media ( 40%)

---
+++ Backend/src/models/Attachment.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const attachmentSchema = new mongoose.Schema({
+    conversationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Conversation',
+        index: true
+    },
+    messageId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message',
+        index: true
+    },
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    type: {
+        type: String,
+        enum: ['image', 'video', 'file']
+    },
+    url: String,
+    meta: {
+        width: Number,
+        height: Number,
+        duration: Number,
+        size: Number
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+attachmentSchema.index({ conversationId: 1, messageId: 1, senderId: 1, createdAt: -1 })
+
+const Attachment = mongoose.model("Attachment", attachmentSchema)
+
+export default Attachment;
--- Backend/src/models/Message.js
+++ Backend/src/models/Message.js
@@ -1,32 +1,43 @@
-import moongose from 'mongoose';
+import mongoose from 'mongoose';
 
-const messageSchema = new moongose.Schema({
+const messageSchema = new mongoose.Schema({
     conversationId: {
-        type: moongose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Conversation',
         require: true,
         index: true
     },
     senderId: {
-        type: moongose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         require: true
     },
     type: {
         type: String,
-        enum: ['text', 'image', 'system'],
+        enum: ['text', 'media', 'mixed', 'system'],
         default: 'text'
     },
     content: {
         type: String
     },
-    imgUrls: [String]
+    mediaIds: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Attachment"
+    }],
+    replyTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message"
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
 }, {
     timestamps: true
 })
 
 messageSchema.index({ conversationId: 1, createdAt: -1 })
 
-const Message = moongose.model("Message", messageSchema)
+const Message = mongoose.model("Message", messageSchema)
 
 export default Message;
