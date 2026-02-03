19b9c0bd65f011c516c1db5293e61cf4e2363a35
DamVanAnh <vananhdam@gmail.com>
2026-02-03 18:16:28 +0700

    feat: send group message with media

--- Backend/src/routes/fileRoute.js
+++ Backend/src/routes/fileRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { deleteFile, getAvatarSignature, getBgSignature, getImageSignature } from '../controllers/fileController.js';
+import { deleteFile, getAvatarSignature, getBgSignature, getMediaSignature } from '../controllers/fileController.js';
 
 const uploadFileRoute = express.Router();
 
 uploadFileRoute.get("/signature/avatar", getAvatarSignature)
 uploadFileRoute.get("/signature/bg", getBgSignature)
-uploadFileRoute.get("/signature/image", getImageSignature)
+uploadFileRoute.get("/signature/media", getMediaSignature)
 uploadFileRoute.delete("/delete", deleteFile)
 
 export default uploadFileRoute;
